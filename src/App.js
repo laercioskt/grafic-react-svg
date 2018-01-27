67ece74a9d5cedfb278ef40a691192ecfd88fe51
Lucas <lucass@gmail.com>
2018-01-27 19:19:56 -0200

    white on gray and initial value

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ class App extends Component {
         this.props.actions.changeFunction(e.target.value); 
     }
 
+    componentDidMount()
+	  {
+	      this.refs.functionInput.value = "x*(x/30)";
+	  }
+
     render() {
         var func = this.props.functionToRender;
         var size = 30;
@@ -19,25 +24,30 @@ class App extends Component {
 
         var valuePoints = "";
         for (var x = 0; x <= size; x+=functionResolution) {
-            valuePoints += x + "," + eval("(function(x) { return " + func + ";})("+ x +")").toFixed(2) + " ";
+            try{
+                var evalResult = eval("(function(x) { return " + func + ";})("+ x +")");
+                valuePoints += x + "," + evalResult.toFixed(2) + " ";
+            }catch(e){
+                valuePoints += x + ",0 ";
+            }
         }
 
         return (
             <div className="App">
-                <p>Função: <input type="text" onBlur={e=>this.onBlur(e)} placeholder="x*(x/30)" /></p>
-                <svg id="resizableSVG" viewBox={"-"+textHorizontalOffset+" 0 "+(size+textHorizontalOffset)+" 30"} height="400" width="400">
+                <p>Função: <input type="text" onBlur={e=>this.onBlur(e)} ref = "functionInput"></input></p>
+                <svg id="resizableSVG" viewBox={"-"+textHorizontalOffset+" 0 "+(size+textHorizontalOffset)+" 30"} height="300" width="300">
 
-                    <text x={"-"+textHorizontalOffset} y={size} font-family="Verdana" font-size="1">0</text>
-                    <text x={"-"+textHorizontalOffset} y={size-5} font-family="Verdana" font-size="1">5</text>
-                    <text x={"-"+textHorizontalOffset} y={size-10} font-family="Verdana" font-size="1">10</text>
-                    <text x={"-"+textHorizontalOffset} y={size-15} font-family="Verdana" font-size="1">15</text>
-                    <text x={"-"+textHorizontalOffset} y={size-20} font-family="Verdana" font-size="1">20</text>
-                    <text x={"-"+textHorizontalOffset} y={size-25} font-family="Verdana" font-size="1">25</text>
-                    <text x={"-"+textHorizontalOffset} y={size-30} font-family="Verdana" font-size="1">30</text>
+                    <text x={"-"+textHorizontalOffset} y={size} font-family="Verdana" font-size="1" stroke="none" fill="white">0</text>
+                    <text x={"-"+textHorizontalOffset} y={size-5} font-family="Verdana" stroke="none" fill="white"font-size="1">5</text>
+                    <text x={"-"+textHorizontalOffset} y={size-10} font-family="Verdana" stroke="none" fill="white"font-size="1">10</text>
+                    <text x={"-"+textHorizontalOffset} y={size-15} font-family="Verdana" stroke="none" fill="white"font-size="1">15</text>
+                    <text x={"-"+textHorizontalOffset} y={size-20} font-family="Verdana" stroke="none" fill="white"font-size="1">20</text>
+                    <text x={"-"+textHorizontalOffset} y={size-25} font-family="Verdana" stroke="none" fill="white"font-size="1">25</text>
+                    <text x={"-"+textHorizontalOffset} y={size-30} font-family="Verdana" stroke="none" fill="white"font-size="1">30</text>
 
                     <g transform={"translate(0,"+size+") scale(1,-1)"}>
                         <Grid width={size} height={size} widthInterval="5" heightInterval="5" />
-                        <polyline points={valuePoints} fill="transparent" stroke="black" strokeWidth="0.1"></polyline>
+                        <polyline points={valuePoints} fill="transparent" stroke="white" strokeWidth="0.1"></polyline>
                     </g>
                 </svg>
 
